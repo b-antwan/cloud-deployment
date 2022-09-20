528ae806d5adcba3dcbe14fb708750d88bb8a1d8
Antoine Blancy <antoine.blancy@etu.hesge.ch>
2022-09-20 15:31:13 +0200

    deployment script skeleton

---
+++ script.py
@@ -0,0 +1,11 @@
+import time
+
+def do_deployment():
+    print('Implement this.')
+    return
+
+if __name__ == '__main__':
+    start = time.time()
+    do_deployment()
+    end = time.time()
+    print("Deployment took " + end-start + "s")

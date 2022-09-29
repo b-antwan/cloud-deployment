dece3e6d420a99d3aa84c364a61b9dd1460a05da
thomas.dagierjo <thomas.dagier-joncour@etu.hesge.ch>
2022-09-30 00:33:40 +0200

    lol

--- thomas/app/README.md
+++ thomas/app/README.md
@@ -15,3 +15,32 @@ moodle:
 INFOS:
 
 Microsoft Azure provides excellent object storage with  Azure Blob Storage, which offers unmatched durability, virtually infinite capacity and multiple tiers of storage, all at very convenient rates. However, because Azure Blob Storage was developed before the world decided to “standardize” on the S3 APIs, the two use different interfaces, and so most applications and libraries designed to work with Amazon S3 do not support Azure out-of-the-box.
+-> https://cloudblogs.microsoft.com/opensource/2017/11/09/s3cmd-amazon-s3-compatible-apps-azure-storage/
+
+
+Deploiement:
+
+1- export AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=cloudlab1;AccountKey=Dj7nJbfXaUy+U8+WEgfX0yB/wJ0k+havtFTLkuZIKAUuAuj+5T6wflQJ5zpQSNxUNrCfTnCZZsLe+AStlP02Vg==;EndpointSuffix=core.windows.net"
+
+
+2- export AZURE_SUBSCRIPTION_ID=f0514094-5bb5-4925-9323-de8fc229bb63
+
+Notes:
+
+- tips -> appeler les VMs "BACK" et "FRONT"
+- se connecter -> dans le terminal: ssh -i thomas-aids.pem azureuser@IP_FRONT (ou IP_BACK)
+- /!\ modifier l'ip du backend dans le script du frontend
+- les SG -> FRONT = inbound source any:* dst any:8000
+            BACK  = inbound source any:* dst any:3000
+
+https://media-exp1.licdn.com/dms/image/D4D03AQFod2XwsQFf2A/profile-displayphoto-shrink_800_800/0/1662067030074?e=1669852800&v=beta&t=2TTtgKV9UM5xcfT20NxII7jSQ90Xvq00G0KkjUyKC3Q
+
+
+Pour lancer le script:
+
+sudo apt install python3.8-venv
+python3 -m venv .venv
+source .venv/bin/activate
+export AZURE_SUBSCRIPTION_ID=f0514094-5bb5-4925-9323-de8fc229bb63
+pip install -r requirements.txt
+python3 startup.py
--- thomas/app/backend/index.js
+++ thomas/app/backend/index.js
@@ -10,31 +10,48 @@ const port = 3000;
 var cors = require('cors')
 app.use(cors());
 
+const { BlobServiceClient } = require('@azure/storage-blob');
+const { v1: uuidv1} = require('uuid');
+require('dotenv').config();
+
+var containerClient;
+var blockBlobClient;
+
 async function getObject(){
-    //To implement -> Must return Promise of {'title': 'blablabla', 'url':'blablabla'}
-    return new Promise((resolve, reject) => {
-        JSON.stringify({'title': 'My awesome timer', 'url': 'https://www.giantfreakinrobot.com/wp-content/uploads/2022/08/rick-astley.jpg'})
-            }).then((state) => {
-                console.log("yeeeees");
-            })
-            .catch((error) => {
-                console.log("aie");
-            });
-    //return new Promise(() => { JSON.stringify({'title': 'My awesome timer', 'url': 'https://www.giantfreakinrobot.com/wp-content/uploads/2022/08/rick-astley.jpg'})})
+    const downloadBlockBlobResponse = await blockBlobClient.download(0);
+    console.log("\nDownloaded blob content...");
+    content = await streamToText(downloadBlockBlobResponse.readableStreamBody);
+    console.log("\t", content);
+
+    async function streamToText(readable) {
+        readable.setEncoding('utf8');
+        let data = '';
+        for await (const chunk of readable) {
+            data += chunk;
+        }
+        return data;
+    }
+
+    return content;
 }
 
-//Obj must be {'title': 'blablabla', 'url':'blablabla'}
 async function setObject(obj){
-    //To implement
+    const blobName = "item_" + uuidv1();
+    blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    console.log(`\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`);
+
+    const data = JSON.stringify(obj);
+    const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+    console.log(`Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`);
+
     return 200;
 }
 
 app.get('/', (req, res) => {
-    /*getObject().then(o => {
+    getObject().then(o => {
         console.log(o)
         res.status(200).send(o)
-    });*/
-    res.status(200).json({'title': 'My awesome timer', 'url': 'https://www.giantfreakinrobot.com/wp-content/uploads/2022/08/rick-astley.jpg'});
+    });
 });
 
 
@@ -42,79 +59,42 @@ app.post('/', (req, res) => {
     setObject(req.body).then(o => res.sendStatus(o));
 });
 
+async function create_azure_storage() {
+    console.log('Azure Blob storage v12 - JavaScript quickstart sample');
+
+    const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+    
+    if(!AZURE_STORAGE_CONNECTION_STRING) {
+        throw Error("Azure Storage Connection string not found");
+    }
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+        AZURE_STORAGE_CONNECTION_STRING
+    );
+  
+    const containerName = "app" + uuidv1();
+        
+    console.log("\nCreating container...");
+    console.log("\t", containerName);
+        
+    containerClient = blobServiceClient.getContainerClient(containerName);
+    const createContainerResponse = await containerClient.create();
+    console.log(`Container was created successfully.\n\trequestId:${createContainerResponse.requestId}\n\tURL: ${containerClient.url}`);
+}
+
+create_azure_storage();
 app.listen(port);
 
-/////////////////////////// TEMPLATE BLOB STORAGE ////////////////////////////////////
+process.on('SIGINT', async function () { 
+    console.log("lol1");
+    const deleteContainerResponse = await containerClient.delete();
+    console.log("Container was deleted successfully. requestId: ", deleteContainerResponse.requestId);
+    process.exit(0);
+});
 
-// TODO -> set la variable d'environement
-const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
-
-if(!AZURE_STORAGE_CONNECTION_STRING) {
-    throw Error("Azure Storage Connection string not found");
-}
-
-// Create the BlobServiceClient object which will be used to create a container client
-const blobServiceClient = BlobServiceClient.fromConnectionString(
-    AZURE_STORAGE_CONNECTION_STRING
-);
-  
-// Create a unique name for the container
-const containerName = "quickstart" + uuidv1();
-  
-console.log("\nCreating container...");
-console.log("\t", containerName);
-  
-/*// Get a reference to a container
-const containerClient = blobServiceClient.getContainerClient(containerName);
-// Create the container
-const createContainerResponse = await containerClient.create();
-console.log(`Container was created successfully.\n\trequestId:${createContainerResponse.requestId}\n\tURL: ${containerClient.url}`);
-
-// Create a unique name for the blob
-const blobName = "quickstart" + uuidv1() + ".txt";
-
-// Get a block blob client
-const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-// Display blob name and url
-console.log(`\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`);
-
-// Upload data to the blob
-const data = "Hello, World!";
-const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-console.log(`Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`);
-
-console.log("\nListing blobs...");
-
-// List the blob(s) in the container.
-for await (const blob of containerClient.listBlobsFlat()) {
-
-    // Get Blob Client from name, to get the URL
-    const tempBlockBlobClient = containerClient.getBlockBlobClient(blob.name);
-
-    // Display blob name and URL
-    console.log(`\n\tname: ${blob.name}\n\tURL: ${tempBlockBlobClient.url}\n`);
-}
-
-// Get blob content from position 0 to the end
-// In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
-// In browsers, get downloaded data by accessing downloadBlockBlobResponse.blobBody
-const downloadBlockBlobResponse = await blockBlobClient.download(0);
-console.log("\nDownloaded blob content...");
-console.log("\t", await streamToText(downloadBlockBlobResponse.readableStreamBody));
-
-// Convert stream to text
-async function streamToText(readable) {
-    readable.setEncoding('utf8');
-    let data = '';
-    for await (const chunk of readable) {
-        data += chunk;
-    }
-    return data;
-}
-
-// Delete container
-console.log("\nDeleting container...");
-
-const deleteContainerResponse = await containerClient.delete();
-console.log("Container was deleted successfully. requestId: ", deleteContainerResponse.requestId);*/
+process.on('SIGTERM', async function () { 
+    console.log("lol2");
+    const deleteContainerResponse = await containerClient.delete();
+    console.log("Container was deleted successfully. requestId: ", deleteContainerResponse.requestId);
+    process.exit(0);
+});
